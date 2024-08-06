de3992717a3a2509b6e1ee4142c6d9fad33012b8
kewen <kauakewen1010@hotmail.com>
2024-08-06 19:01:44 -0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-"use client";
+"use client"
 
-import { useState } from "react";
-import { buffer } from "stream/consumers";
-import { Button } from "./_components/ui/button";
+import { Button } from "./_components/ui/button"
 
 // SERVER COMPONENTS
+
 const Home = () => {
   return <Button>test</Button>
-};
+}
 
-export default Home;
+export default Home
